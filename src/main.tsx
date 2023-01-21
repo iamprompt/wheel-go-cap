e25d4fc33a9b8ba4276767ad0efa4d7d46731570
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-01-22 05:14:12 +0700

    feat:linting

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
+import './styles/globals.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
+
 import HelloPage from './pages/hello'
 import HiPage from './pages/hi'
-
-import './styles/globals.css'
 
 const router = createBrowserRouter([
   {
