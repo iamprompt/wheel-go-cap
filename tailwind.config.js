f479d77a07c9c41b6d56c2e2492ca6e0da5fac27
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-01-25 01:22:12 +0700

    add line fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
   theme: {
+    fontFamily: {
+      sans: ['LINE Seed Sans TH', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [
