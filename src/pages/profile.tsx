0d8b509d70190bdc57bf5c8af22f61f470bce2d9
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-01-26 13:16:22 +0700

    edit profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -3,44 +3,44 @@ import { HomeLayout } from '../layouts/home'
 const ProfilePage = () => {
   return (
     <HomeLayout className="py-16">
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
-      <div>Profile</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
+      <div>Profile 1 1</div>
     </HomeLayout>
   )
 }
