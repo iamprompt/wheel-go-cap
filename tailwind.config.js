523989d99c0654590b48f8a97f2dd68fd4f0a462
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-01-29 00:18:45 +0700

    remove unused var

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ const config = {
     extend: {},
   },
   plugins: [
-    plugin(function ({ addUtilities, addBase, config }) {
+    plugin(function ({ addUtilities }) {
       const newUtilities = {
         '.safe-top': {
           paddingTop: 'env(safe-area-inset-top, 16px)',
