1e8e1198e076f3a9fad4534769237a4d2a64bd8a
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-01-29 17:31:37 +0700

    add activities detail

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,35 @@
+import accessibleForwardIcon from '@iconify/icons-material-symbols/accessible-forward-rounded'
 import navigationIcon from '@iconify/icons-material-symbols/navigation-rounded'
 import rateReviewIcon from '@iconify/icons-material-symbols/rate-review-rounded'
+import { Icon, IconifyIcon } from '@iconify/react'
+import { FC } from 'react'
 import { Link } from 'react-router-dom'
 
 import { BadgeWithLabel } from '../components/Profile/BadgeWithLabel'
 import { HomeLayout } from '../layouts/home'
+
+type ActivityDetailItemProps = {
+  icon: IconifyIcon
+  label: string
+  value: string
+}
+
+const ActivityDetailItem: FC<ActivityDetailItemProps> = ({
+  icon,
+  label,
+  value,
+}) => {
+  return (
+    <div className="flex flex-row justify-between gap-5">
+      <div className="flex flex-row items-center gap-2">
+        <Icon icon={icon} className="h-5 w-5 text-theme-magenta" />
+        <div className="text-xs font-bold text-theme-magenta">{label}</div>
+      </div>
+
+      <div className="text-xs font-bold text-theme-magenta">{value}</div>
+    </div>
+  )
+}
 
 const ProfilePage = () => {
   return (
@@ -51,6 +77,64 @@ const ProfilePage = () => {
           <BadgeWithLabel size={48} color="#08CE57" />
         </div>
         <hr />
+        <div>
+          <div className="relative h-8 overflow-hidden">
+            <Icon
+              icon={accessibleForwardIcon}
+              className="absolute h-8 w-8 text-qac-magenta"
+              style={{
+                left: 'calc(100% * (20 / 100) - 32px)',
+              }}
+            />
+          </div>
+          <div className="relative overflow-hidden rounded-full">
+            <div className="h-2 bg-theme-smokeWhite" />
+            <div
+              className="absolute top-0 left-0 h-2 bg-qac-magenta"
+              style={{
+                width: 'calc(100% * (20 / 100))',
+              }}
+            />
+          </div>
+          <div className="mt-2 flex flex-row justify-between">
+            <div className="space-x-1 text-xs font-bold">
+              <span className="text-theme-magenta">XP 1,000</span>
+              <span className="text-theme-magenta">/</span>
+              <span className="text-theme-description">2,000</span>
+            </div>
+            <div className="space-x-1 text-xs font-bold">
+              <span className="text-theme-magenta">1,000 XP</span>
+              <span className="text-theme-description">to level up</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="h-3 bg-theme-smokeWhite" />
+
+      <div className="space-y-6 p-6 px-4">
+        <div className="font-bold">Total Activity</div>
+        <div className="space-y-4">
+          <ActivityDetailItem
+            icon={navigationIcon}
+            label="Total Navigation"
+            value="20"
+          />
+          <ActivityDetailItem
+            icon={navigationIcon}
+            label="Total Navigation"
+            value="20"
+          />
+          <ActivityDetailItem
+            icon={navigationIcon}
+            label="Total Navigation"
+            value="20"
+          />
+          <ActivityDetailItem
+            icon={navigationIcon}
+            label="Total Navigation"
+            value="20"
+          />
+        </div>
       </div>
     </HomeLayout>
   )
